68c6a9f053c3434dc9c2c84447f40a61665fbfe4
SachinGadge13 <gadges14@gmail.com>
2022-10-11 18:12:42 +0530

    Added:Welcome Message

---
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.employeewage;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+		
+	}
+
+}
